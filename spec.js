daa7a876a005365fdae568edd54ea154b0119abf
ozornin <a.a.ozornin@tmn2.etagi.com>
2018-04-10 16:27:56 +0500

    done

--- spec.js
+++ spec.js
@@ -1,3 +1,7 @@
 describe('angularjs homepage todo list', function() {
-    
+    it('Firts Test', function() {
+        browser.waitForAngularEnabled(false);
+        browser.get('https://www.etagi.com/');
+        expect(browser.getCurrentUrl()).toBe('https://www.etagi.com/');
+    });
 });
