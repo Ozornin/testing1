5a275ee493705bdede6403e93316ee159fa89ccd
ozornin <a.a.ozornin@tmn2.etagi.com>
2018-04-21 10:51:37 +0500

    done

--- specStickers.js
+++ specStickers.js
@@ -2,16 +2,21 @@ describe('angularjs homepage todo list', function() {
   it('Auth', function () {
     browser.waitForAngularEnabled(false);
     browser.get('http://localhost/litecart');
-    expect($$('#box-most-popular li:first-child div.sticker').count()).toBe(1);
-    expect($$('#box-most-popular li:nth-child(2) div.sticker').count()).toBe(1);
-    expect($$('#box-most-popular li:nth-child(3) div.sticker').count()).toBe(1);
-    expect($$('#box-most-popular li:nth-child(4) div.sticker').count()).toBe(1);
-    expect($$('#box-most-popular li:nth-child(5) div.sticker').count()).toBe(1);
-    expect($$('#box-campaigns li:first-child div.sticker').count()).toBe(1);
-    expect($$('#box-latest-products li:first-child div.sticker').count()).toBe(1);
-    expect($$('#box-latest-products li:nth-child(2) div.sticker').count()).toBe(1);
-    expect($$('#box-latest-products li:nth-child(3) div.sticker').count()).toBe(1);
-    expect($$('#box-latest-products li:nth-child(4) div.sticker').count()).toBe(1);
-    expect($$('#box-latest-products li:nth-child(5) div.sticker').count()).toBe(1);
+    var popular = $$('#box-most-popular li');
+    var campaigns = $$('box-campaigns');
+    var latest = $$('box-latest-products');
+    var sticker;
+    popular.each(function(object) {
+      sticker = object.$$('div.sticker');
+      expect(sticker.count()).toBe(1);
+    });
+    campaigns.each(function(object) {
+      sticker = object.$$('div.sticker');
+      expect(sticker.count()).toBe(1);
+    });
+    latest.each(function(object) {
+      sticker = object.$$('div.sticker');
+      expect(sticker.count()).toBe(1);
+    });
   });
 });
