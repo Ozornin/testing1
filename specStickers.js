44e0aed343e4593f0ef2af70364d2cc6f16fcd70
ozornin <a.a.ozornin@tmn2.etagi.com>
2018-04-21 13:10:40 +0500

    done

--- specStickers.js
+++ specStickers.js
@@ -2,21 +2,12 @@ describe('angularjs homepage todo list', function() {
   it('Auth', function () {
     browser.waitForAngularEnabled(false);
     browser.get('http://localhost/litecart');
-    var popular = $$('#box-most-popular li');
-    var campaigns = $$('#box-campaigns li');
-    var latest = $$('#box-latest-products li');
+    var object = $$('li.product.column');
     var sticker;
-    popular.each(function(object) {
+    object.each(function(object) {
       sticker = object.$$('div.sticker');
       expect(sticker.count()).toBe(1);
     });
-    campaigns.each(function(object) {
-      sticker = object.$$('div.sticker');
-      expect(sticker.count()).toBe(1);
-    });
-    latest.each(function(object) {
-      sticker = object.$$('div.sticker');
-      expect(sticker.count()).toBe(1);
-    });
+
   });
 });
