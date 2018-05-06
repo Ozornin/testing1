22711a26e30ffe68dcf697ef826af6f9cfa9dd37
ozornin <a.a.ozornin@tmn2.etagi.com>
2018-05-06 15:21:21 +0500

    done

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['specAddProduct.js'],
+    specs: ['specBasket.js'],
     framework: 'jasmine2',
     multiCapabilities: [{
       browserName: 'chrome'
