030a743a9a3ac6f373efdb0e6734b0010f429407
ozornin <a.a.ozornin@tmn2.etagi.com>
2018-05-13 14:26:14 +0500

    done

--- specBasket/specBasket.js
+++ specBasket/specBasket.js
@@ -2,7 +2,7 @@ const addStep = Object.create(require('../steps/addProduct.js'));
 const deleteStep = Object.create(require('../steps/deleteProduct.js'));
 
 describe('angularjs homepage todo list', function() {
-  it('Auth', function() {
+  it('addProduct', function() {
     browser.waitForAngularEnabled(false);
     addStep.addProductStep();
     deleteStep.deleteProductStep();
